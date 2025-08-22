379051ac6ff6992fad44d48607542ef8dc958412
maouchezina <maoucheziina@gmail.com>
2025-08-22 19:04:33 +0100

    modification env

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "http://localhost:5000";
+const API_URL = "https://back-umber-theta.vercel.app";
 
 const instance = axios.create({ baseURL: API_URL });
 
